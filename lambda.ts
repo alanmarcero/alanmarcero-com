0e33cbba27e7a388bd0714553a13634c27a09ecb
Alan Marcero <alanmarcero@gmail.com>
2025-10-05 17:03:08 -0400

    cleanup ts. add empty react

--- lambda.ts
+++ lambda.ts
@@ -31,34 +31,18 @@ interface YouTubeRes {
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   const apiUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${apiKey}`;
 
-
   const [fetchErr, response] = await asyncWrapper(fetch(apiUrl));
-  if (fetchErr || !response) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Failed to fetch from YouTube", details: fetchErr?.message }),
-    };
-  }
+  if (fetchErr || !response || !response.ok)
+    return { statusCode: 500, body: JSON.stringify({ error: "YouTube Fetch Failed", details: fetchErr?.message }) };
 
-  if (!response.ok) {
-    return {
-      statusCode: response.status,
-      body: JSON.stringify({ error: `Failed at fetching: ${response.statusText}` })
-    };
-  }
-
-  const [parseErr, data] = await asyncWrapper<YouTubeRes>(response.json());
-  if (parseErr || !data) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: `Failed at parsing: ${parseErr?.message}` })
-    };
-  }
+  const [parseErr, playList] = await asyncWrapper<YouTubeRes>(response.json());
+  if (parseErr || !playList)
+    return { statusCode: 500, body: JSON.stringify({ error: `Parse Failed: ${parseErr?.message}` }) }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      items: data.items.map(item => ({
+      items: playList.items.map(item => ({
         title: item.snippet.title,
         videoId: item.snippet.resourceId.videoId,
         publishedAt: item.snippet.publishedAt,
